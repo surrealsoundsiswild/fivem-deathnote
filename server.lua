76010fd2ab7708dd6955a7fe6c0edfe70504e0e4
surrealsoundsiswild <jakefastnergta5rp@gmail.com>
2025-02-27 04:30:25 -0600

    Add files via upload

--- server.lua
+++ server.lua
@@ -1,24 +1,18 @@
--- Death Note Server Script
 
--- Event handler for using the death note
 RegisterServerEvent('deathnote:useDeathNote')
 AddEventHandler('deathnote:useDeathNote', function(targetId)
     local source = source
     local targetPlayer = tonumber(targetId)
     
-    -- Check if target player exists
     if GetPlayerName(targetPlayer) then
-        -- Inform the user that the death note was used
         TriggerClientEvent('chat:addMessage', source, {
             color = {255, 0, 0},
             multiline = true,
             args = {"Death Note", "You have written " .. GetPlayerName(targetPlayer) .. "'s name in the Death Note."}
         })
         
-        -- Trigger the death note effect on the target player
         TriggerClientEvent('deathnote:executeDeathNote', targetPlayer)
     else
-        -- Inform the user if the target player doesn't exist
         TriggerClientEvent('chat:addMessage', source, {
             color = {255, 0, 0},
             multiline = true,
@@ -27,8 +21,6 @@ AddEventHandler('deathnote:useDeathNote', function(targetId)
     end
 end)
 
--- Add ACL permission check (optional, uncomment to use)
---[[
 AddEventHandler('chatMessage', function(source, name, message)
     if string.sub(message, 1, 10) == '/deathnote ' then
         -- Check if player has permission to use the command
@@ -42,4 +34,3 @@ AddEventHandler('chatMessage', function(source, name, message)
         end
     end
 end)
---]]
